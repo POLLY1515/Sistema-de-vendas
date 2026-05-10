2486983b89d16c0bacdfa00c8d573cdf094f982e
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-09 23:04:57 -0300

    GlobalExceptionHandler

--- backend/src/main/java/br/com/lacasa/sistemavendas/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/exception/GlobalExceptionHandler.java
@@ -1,5 +1,69 @@
 package br.com.lacasa.sistemavendas.exception;
 
-public class GlobalExceptionHandler {
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.lacasa.sistemavendas.dto.ErroResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	
+	@ExceptionHandler(RecursoNaoEncontradoException.class)
+	public ResponseEntity<ErroResponseDTO> tratarRecursoNaoEncontrado(
+			RecursoNaoEncontradoException ex,
+			HttpServletRequest request 
+	
+
+) {
+	ErroResponseDTO erro = new ErroResponseDTO(
+			LocalDateTime.now(),
+			HttpStatus.NOT_FOUND.value(),
+			"Not found",
+			ex.getMessage(),
+			request.getRequestURI()
+	);
+	
+	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 }
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, Object>> tratarErroDevalidacao(
+			MethodArgumentNotValidException ex,
+			HttpServletRequest request
+			
+		) {
+		
+		Map<String, String> campos = new HashMap<>();
+		
+		ex.getBindingResult().getFieldErrors().forEach(erro -> {
+			campos.put(erro.getField(), erro.getDefaultMessage());
+			
+		});
+		
+		Map<String, Object> resposta = new HashMap<>();
+		resposta.put("dataHora", LocalDateTime.now());
+		resposta.put("status", HttpStatus.BAD_REQUEST.value());
+		resposta.put("erro", "Bad Request");
+		resposta.put("mensagem", "Existem campos inválidos");
+		resposta.put("caminho", request.getRequestURI());
+		resposta.put("campos", campos);
+		
+		return ResponseEntity.badRequest().body(resposta);
+		
+		
+		}
+	
+	}
+	
+	
+	
