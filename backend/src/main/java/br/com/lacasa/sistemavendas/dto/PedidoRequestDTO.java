052dd8168d1939041ebb2a86325564603981d281
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-16 22:17:55 -0300

    PedidoRequestDTO

--- backend/src/main/java/br/com/lacasa/sistemavendas/dto/PedidoRequestDTO.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/dto/PedidoRequestDTO.java
@@ -1,5 +1,21 @@
 package br.com.lacasa.sistemavendas.dto;
 
-public class PedidoRequestDTO {
+import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record PedidoRequestDTO(
+		
+		@NotNull(message = "O cliente é obrigatorio")
+		Long clienteId,
+		
+		@NotEmpty(message = "O pedido precisa ter pelo menos um item")
+		List<@Valid ItemPedidoRequestDTO> itens
+		
+		
+) {
+
+	
 }
