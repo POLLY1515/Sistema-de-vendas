5dd73a7c437c4b719d1c93d298d77f30c713e622
POLLY1515 <amarantepolly2030@gmail.com>
2026-06-14 22:31:48 -0300

    Metodo deletar

--- backend/src/main/java/br/com/lacasa/sistemavendas/controller/ClienteController.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,9 @@ public class ClienteController {
 		return ResponseEntity.ok(cliente);
 	}
 	
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id){
+		clienteService.remover(id);
+		return ResponseEntity.noContent().build();
+	}
 }
