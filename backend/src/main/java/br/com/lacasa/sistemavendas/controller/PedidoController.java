ebb1055d52f5524f71b6a6fecf3ac8d33e783455
POLLY1515 <amarantepolly2030@gmail.com>
2026-06-18 09:17:54 -0300

    Buscar por id e listar todos

--- backend/src/main/java/br/com/lacasa/sistemavendas/controller/PedidoController.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/controller/PedidoController.java
@@ -1,7 +1,11 @@
 package br.com.lacasa.sistemavendas.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +30,15 @@ public class PedidoController {
 	public ResponseEntity<PedidoResponseDTO> criar(@RequestBody @Valid PedidoRequestDTO request){
 		PedidoResponseDTO response = pedidoService.criarPedido(request);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PedidoResponseDTO>buscarPorId(@PathVariable Long id){
+		return ResponseEntity.ok(pedidoService.buscarPorId(id));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<PedidoResponseDTO>>listarTodos(){
+		return ResponseEntity.ok(pedidoService.listarTodos());
 	}
 }
