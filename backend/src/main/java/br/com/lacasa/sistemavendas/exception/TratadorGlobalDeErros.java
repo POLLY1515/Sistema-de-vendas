a030b2ac5e9e2bda6a88e1daf40c2bff9cbe3cae
POLLY1515 <amarantepolly2030@gmail.com>
2026-06-22 13:33:55 -0300

    Tratador global de erros

---
+++ backend/src/main/java/br/com/lacasa/sistemavendas/exception/TratadorGlobalDeErros.java
@@ -0,0 +1,26 @@
+package br.com.lacasa.sistemavendas.exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorGlobalDeErros {
+	
+	@ExceptionHandler(EstoqueInsuficienteException.class)
+public ResponseEntity<Map<String, Object>> tratarEstoqueInsuficiente(
+		EstoqueInsuficienteException erro
+		
+		){
+		Map<String, Object> resposta = Map.of(
+				"dataHora", LocalDateTime.now(),
+				"status",400,
+				"erro", "Estoque insufiente",
+				"mensagem", erro.getMessage()
+				);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+	}
+}
