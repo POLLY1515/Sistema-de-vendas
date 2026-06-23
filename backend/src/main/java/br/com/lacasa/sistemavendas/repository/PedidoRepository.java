c194052f6150805a61100623a583624686d28379
POLLY1515 <amarantepolly2030@gmail.com>
2026-06-22 22:32:26 -0300

    PedidoRepository

--- backend/src/main/java/br/com/lacasa/sistemavendas/repository/PedidoRepository.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/repository/PedidoRepository.java
@@ -1,9 +1,27 @@
 package br.com.lacasa.sistemavendas.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lacasa.sistemavendas.entity.Pedido;
+import br.com.lacasa.sistemavendas.entity.StatusPedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+	List<Pedido> findByStatus(StatusPedido status);
+	
+	List<Pedido> findByClienteId(Long clienteId);
+	
+	List<Pedido> findByDataCriacaoBetween(
+			LocalDateTime inicio,
+			LocalDateTime fim
+			);
+	
+	List<Pedido> findByStatusAndDataCriacaoBetween(
+				StatusPedido status,
+				LocalDateTime inicio,
+				LocalDateTime fim
+				);
 }
