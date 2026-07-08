f9dda0c4ebea9d20e9da310fa1a62e7f76b662ee
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-08 10:20:48 -0300

    Modificando ItemPedidoResponseDTO

--- backend/src/main/java/br/com/lacasa/sistemavendas/dto/ItemPedidoResponseDTO.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/dto/ItemPedidoResponseDTO.java
@@ -2,15 +2,25 @@ package br.com.lacasa.sistemavendas.dto;
 
 import java.math.BigDecimal;
 
-public record ItemPedidoResponseDTO(
-		
-		Long produtoId,
-		String nomeProduto,
-		Integer quantidade,
-		BigDecimal precoUnitario,
-		BigDecimal subTotal
-		
-		
-		) {
+import br.com.lacasa.sistemavendas.entity.ItemPedido;
 
+public record ItemPedidoResponseDTO(
+
+        Long produtoId,
+        String nomeProduto,
+        Integer quantidade,
+        BigDecimal precoUnitario,
+        BigDecimal subTotal
+
+) {
+
+    public ItemPedidoResponseDTO(ItemPedido item) {
+        this(
+                item.getProduto().getId(),
+                item.getProduto().getNome(),
+                item.getQuantidade(),
+                item.getPrecoUnitario(),
+                item.getSubtotal()
+        );
+    }
 }
