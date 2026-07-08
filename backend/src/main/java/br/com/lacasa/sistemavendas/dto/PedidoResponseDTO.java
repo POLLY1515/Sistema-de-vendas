3a64fb223e823ac409f25a29ca54a271ae1defbc
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-08 10:19:40 -0300

    Atualizando classe PedidoResponseDTO

--- backend/src/main/java/br/com/lacasa/sistemavendas/dto/PedidoResponseDTO.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/dto/PedidoResponseDTO.java
@@ -4,22 +4,35 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import br.com.lacasa.sistemavendas.entity.Pedido;
 import br.com.lacasa.sistemavendas.entity.StatusPedido;
 
 public record PedidoResponseDTO(
-		
-		Long id,
-		Long clienetId,
-		String nomeCliente,
-		LocalDateTime dataPedido,
-		StatusPedido status,
-		LocalDateTime dataCriacao,
-		BigDecimal valorTotal,
-		List<ItemPedidoResponseDTO>itens
 
-		
-		
-		) {
-	
+        Long id,
+        Long clienteId,
+        String nomeCliente,
+        LocalDateTime dataPedido,
+        StatusPedido status,
+        LocalDateTime dataCriacao,
+        BigDecimal valorTotal,
+        List<ItemPedidoResponseDTO> itens
 
+) {
+
+    public PedidoResponseDTO(Pedido pedido) {
+        this(
+                pedido.getId(),
+                pedido.getCliente().getId(),
+                pedido.getCliente().getNome(),
+                pedido.getDataPedido(),
+                pedido.getStatus(),
+                pedido.getDataCriacao(),
+                pedido.getValorTotal(),
+                pedido.getItens()
+                        .stream()
+                        .map(ItemPedidoResponseDTO::new)
+                        .toList()
+        );
+    }
 }
