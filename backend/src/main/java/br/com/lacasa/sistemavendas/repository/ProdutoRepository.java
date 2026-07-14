27546f6d69e58c12c864d34201ef8fcb9dadb7ad
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-13 22:18:57 -0300

    findByNomeContainingIgnoreCase

--- backend/src/main/java/br/com/lacasa/sistemavendas/repository/ProdutoRepository.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/repository/ProdutoRepository.java
@@ -1,9 +1,18 @@
 package br.com.lacasa.sistemavendas.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lacasa.sistemavendas.entity.Produto;
 
 public interface ProdutoRepository  extends JpaRepository<Produto, Long>{
 
+	boolean existsBynomeIgnoreCase(String nome);
+	
+	Page<Produto> findByNomeContainingIgnoreCase(
+			String nome,
+			Pageable pageable
+			
+			);
 }
