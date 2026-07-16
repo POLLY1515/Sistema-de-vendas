a6d0083908418c4d6705093474cb57b94881f168
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-16 10:53:25 -0300

    long totalElementos

--- backend/src/main/java/br/com/lacasa/sistemavendas/dto/PaginaResponseDTO.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/dto/PaginaResponseDTO.java
@@ -2,11 +2,14 @@ package br.com.lacasa.sistemavendas.dto;
 
 import java.util.List;
 
+
+
+
 public record PaginaResponseDTO<T>(
 		List<T> conteudo,//pode ser uma lista de clientes, produtos ou pedidos
 		int paginaAtual,
 		int tamanhoPagina,
-		int totalElementos,
+		long totalElementos,
 		int totalPaginas,
 		boolean primeira,
 		boolean ultima) {
