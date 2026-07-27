ae56c6048a6a240d54138e98d7ffafc872b79637
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-27 15:28:37 -0300

    validarQuantidadePositiva

--- backend/src/main/java/br/com/lacasa/sistemavendas/entity/Produto.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/entity/Produto.java
@@ -3,6 +3,7 @@ package br.com.lacasa.sistemavendas.entity;
 import java.math.BigDecimal;
 
 import br.com.lacasa.sistemavendas.exception.EstoqueInsuficienteException;
+import br.com.lacasa.sistemavendas.exception.RegraNegocioException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,47 +15,47 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Entity
-@Table(name= "produtos")
+@Table(name = "produtos")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
-
 public class Produto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable = false, length = 100)
-	private String nome;
-	
-	@Column(nullable = false, precision = 10, scale = 2)
-	private BigDecimal preco;
-	
-	@Column(nullable = false)
-	private Integer quantidadeEstoque = 0;
-	
-	
-	
-	
-	public void baixarEstoque(Integer quantidadeVendida) {
-		if(quantidadeVendida > this.quantidadeEstoque) {
-			throw new EstoqueInsuficienteException("Estoque insuficiente para o produto" + this.nome);
-		}
-		this.quantidadeEstoque = this.quantidadeEstoque - quantidadeVendida;
-	}
-	
-	
-	public void devolverEstoque(Integer quantidadeDevolvida) {
-		this.quantidadeEstoque = this.quantidadeEstoque + quantidadeDevolvida;
-	}
-	
-	
-	
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer quantidadeEstoque = 0;
+
+    public void baixarEstoque(Integer quantidadeVendida) {
+        validarQuantidadePositiva(quantidadeVendida);
+
+        if (quantidadeVendida > this.quantidadeEstoque) {
+            throw new EstoqueInsuficienteException(
+                    "Estoque insuficiente para o produto " + this.nome + "."
+            );
+        }
+
+        this.quantidadeEstoque -= quantidadeVendida;
+    }
+
+    public void devolverEstoque(Integer quantidadeDevolvida) {
+        validarQuantidadePositiva(quantidadeDevolvida);
+        this.quantidadeEstoque += quantidadeDevolvida;
+    }
+
+    private void validarQuantidadePositiva(Integer quantidade) {
+        if (quantidade == null || quantidade <= 0) {
+            throw new RegraNegocioException("A quantidade deve ser maior que zero.");
+        }
+    }
 }
