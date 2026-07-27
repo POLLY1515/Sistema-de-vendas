1212fd57adedb3823000d29f0cd0c79e7be3af70
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-27 14:52:17 -0300

    Atualizando ClienteRepository

--- backend/src/main/java/br/com/lacasa/sistemavendas/repository/ClienteRepository.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/repository/ClienteRepository.java
@@ -1,11 +1,20 @@
 package br.com.lacasa.sistemavendas.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lacasa.sistemavendas.entity.Cliente;
 
 public interface ClienteRepository  extends JpaRepository<Cliente, Long>{
 
-	boolean existsByEmail(String email);
 	boolean existsByCpf(String cpf);
+	
+	boolean existsByEmailIgnoreCase(String email);
+	
+	Page<Cliente> findByNomeContainingIgnoreCaseOrEmailContainingIgnoreCase(
+		    String nome,
+		    String email,
+		    Pageable pageable
+		);
 }
