8af60294d36be201839af4545e78ecd32ca0cfd4
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-27 15:45:37 -0300

    existsByEmailIgnoreCaseAndIdNo

--- backend/src/main/java/br/com/lacasa/sistemavendas/repository/ClienteRepository.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/repository/ClienteRepository.java
@@ -6,15 +6,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lacasa.sistemavendas.entity.Cliente;
 
-public interface ClienteRepository  extends JpaRepository<Cliente, Long>{
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-	boolean existsByCpf(String cpf);
-	
-	boolean existsByEmailIgnoreCase(String email);
-	
-	Page<Cliente> findByNomeContainingIgnoreCaseOrEmailContainingIgnoreCase(
-		    String nome,
-		    String email,
-		    Pageable pageable
-		);
+    boolean existsByEmailIgnoreCase(String email);
+
+    boolean existsByCpf(String cpf);
+
+    boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
+
+    boolean existsByCpfAndIdNot(String cpf, Long id);
+
+    Page<Cliente> findByNomeContainingIgnoreCaseOrEmailContainingIgnoreCase(
+            String nome,
+            String email,
+            Pageable pageable
+    );
 }
