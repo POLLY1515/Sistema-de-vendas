f846c5c082dcb0bfa49663c72ac7b323e5305bf8
POLLY1515 <amarantepolly2030@gmail.com>
2026-07-27 22:47:15 -0300

    Usuario

---
+++ backend/src/main/java/br/com/lacasa/sistemavendas/entity/Usuario.java
@@ -0,0 +1,45 @@
+package br.com.lacasa.sistemavendas.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 120)
+	private String nome;
+	
+	@Column(nullable = false, unique = true, length = 160)
+	private String email;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 30)
+	private PerfilUsuario perfil;
+	
+	@Column(nullable = false)
+	private boolean ativo = true;
+	
+	@Column(nullable = false)
+	private LocalDateTime dataCriacao = LocalDateTime.now();
+}
