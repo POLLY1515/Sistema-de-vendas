8c9efa8abf9415e79289a73f154d33af2631737b
POLLY1515 <amarantepolly2030@gmail.com>
2026-08-08 23:52:04 -0300

    Atualizando AuthService

--- backend/src/main/java/br/com/lacasa/sistemavendas/service/AuthService.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/service/AuthService.java
@@ -1,69 +1,67 @@
 package br.com.lacasa.sistemavendas.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.lacasa.sistemavendas.dto.LoginResponseDTO;
 import br.com.lacasa.sistemavendas.dto.UsuarioCadastroRequestDTO;
 import br.com.lacasa.sistemavendas.dto.UsuarioLoginRequestDTO;
 import br.com.lacasa.sistemavendas.dto.UsuarioResponseDTO;
-import br.com.lacasa.sistemavendas.dto.UsuarioResponseDTO.LoginResponseDTO;
 import br.com.lacasa.sistemavendas.entity.Usuario;
 import br.com.lacasa.sistemavendas.repository.UsuarioRepository;
+import br.com.lacasa.sistemavendas.security.JwtService;
 import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-    private final UsuarioRepository usuarioRepository;
-    private final PasswordEncoder passwordEncoder;
+	private final UsuarioRepository usuarioRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
 
-    public AuthService(
-            UsuarioRepository usuarioRepository,
-            PasswordEncoder passwordEncoder) {
-        
-        this.usuarioRepository = usuarioRepository;  // ✅ PONTO-E-VÍRGULA
-        this.passwordEncoder = passwordEncoder;
-    }
+	public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, JwtService jwtService
 
-    @Transactional
-    public UsuarioResponseDTO cadastrar(UsuarioCadastroRequestDTO dto) {
-        if(usuarioRepository.existsByEmailIgnoreCase(dto.email())) {
-            throw new RuntimeException("Já existe usuario com este email");
-        }
+	) {
+		this.usuarioRepository = usuarioRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.jwtService = jwtService;
+	}
 
-        Usuario usuario = new Usuario();
-        usuario.setNome(dto.nome());
-        usuario.setEmail(dto.email());
-        usuario.setSenha(passwordEncoder.encode(dto.senha()));
-        usuario.setPerfil(dto.perfil());
-        usuario.setAtivo(true);
+	@Transactional
+	public UsuarioResponseDTO cadastrar(UsuarioCadastroRequestDTO dto) {
+		if (usuarioRepository.existsByEmailIgnoreCase(dto.email())) {
+			throw new RuntimeException("Já existe usuario com este email");
+		}
 
-        Usuario salvo = usuarioRepository.save(usuario);
-        return transformarEmResponse(salvo);
-    }
+		Usuario usuario = new Usuario();
+		usuario.setNome(dto.nome());
+		usuario.setEmail(dto.email());
+		usuario.setSenha(passwordEncoder.encode(dto.senha()));
+		usuario.setPerfil(dto.perfil());
+		usuario.setAtivo(true);
 
-    public LoginResponseDTO login(UsuarioLoginRequestDTO dto) {
-        Usuario usuario = usuarioRepository.findByEmailIgnoreCase(dto.email())
-                .orElseThrow(() -> new RuntimeException("E-mail ou senha inválidos"));
+		Usuario salvo = usuarioRepository.save(usuario);
+		return transformarEmResponse(salvo);
+	}
 
-        boolean senhaCorreta = passwordEncoder.matches(dto.senha(), usuario.getSenha());
+	public LoginResponseDTO login(UsuarioLoginRequestDTO dto) {
+		Usuario usuario = usuarioRepository.findByEmailIgnoreCase(dto.email())
+				.orElseThrow(() -> new RuntimeException("E-mail ou senha inválidos"));
 
-        if(!senhaCorreta || !usuario.getAtivo()) {
-            throw new RuntimeException("Email ou senha inválidos");
-        }
+		boolean senhaCorreta = passwordEncoder.matches(dto.senha(), usuario.getSenha());
 
-        return new LoginResponseDTO(
-                "Login realizado com sucesso",
-                transformarEmResponse(usuario)
-        );
-    }
+		if (!senhaCorreta || !usuario.getAtivo()) {
+			throw new RuntimeException("Email ou senha inválidos");
+		}
+		
+		String token = jwtService.gerarToken(usuario);
 
-    private UsuarioResponseDTO transformarEmResponse(Usuario usuario) {
-        return new UsuarioResponseDTO(
-                usuario.getId(),
-                usuario.getNome(),
-                usuario.getEmail(),
-                usuario.getPerfil(),
-                usuario.getAtivo()
-        );
-    }
+		return new LoginResponseDTO(token,"Bearer", transformarEmResponse(usuario));
+		//return new LoginResponseDTO(token, "Bearer", transformarEmResponse(usuario));
+	}
+
+	private UsuarioResponseDTO transformarEmResponse(Usuario usuario) {
+		return new UsuarioResponseDTO(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getPerfil(),
+				usuario.getAtivo());
+	}
 }
