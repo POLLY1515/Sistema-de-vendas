7fb8ea6a3a4945baac6e9ea3c6d7fb6fab02ac1e
POLLY1515 <amarantepolly2030@gmail.com>
2026-08-12 19:16:58 -0300

    Atualizando securityConfig  para aceitar CORS

--- backend/src/main/java/br/com/lacasa/sistemavendas/config/SecurityConfig.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.lacasa.sistemavendas.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,9 +22,6 @@ import br.com.lacasa.sistemavendas.security.JwtService;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-
-	
-
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -37,28 +35,23 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter
-) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
 
 		return http
 
 				.csrf(csrf -> csrf.disable())
-
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
+				.cors(Customizer.withDefaults())
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
-
 						.requestMatchers(HttpMethod.POST, "/auth/cadastrar", "/auth/login").permitAll()
+						.requestMatchers(HttpMethod.OPTIONS, "/***").permitAll()
 						.requestMatchers("/postgres/***").permitAll()
-						
-						.anyRequest().authenticated())
-
-				.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
-
+						.anyRequest().authenticated()
+						)
+						.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
 				.httpBasic(httpBasic -> httpBasic.disable())
-
 				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-
 				.build();
 	}
 }
