b0bb134a457801154e53cc95a967bd8f9a51d35c
POLLY1515 <amarantepolly2030@gmail.com>
2026-09-18 08:49:03 -0300

    PESSIMISTIC_WRITE

--- backend/src/main/java/br/com/lacasa/sistemavendas/repository/ProdutoRepository.java
+++ backend/src/main/java/br/com/lacasa/sistemavendas/repository/ProdutoRepository.java
@@ -1,10 +1,16 @@
 package br.com.lacasa.sistemavendas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.lacasa.sistemavendas.entity.Produto;
+import jakarta.persistence.LockModeType;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
@@ -13,4 +19,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     boolean existsByNomeIgnoreCaseAndIdNot(String nome, Long id);
 
     Page<Produto> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Produto p where p.id = :id")
+    Optional<Produto> findByIdParaAtualizacao(@Param("id") Long id);
 }
